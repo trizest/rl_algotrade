530e260e993ba082831b04809649530825198b71
trizest <josh_mutch@hotmail.com>
2021-06-27 18:56:57 +1000

    add tech indicator function

--- 3_process.py
+++ 3_process.py
@@ -1,5 +1,6 @@
 import os 
 import pandas as pd
+from finta import TA
 import numpy as np
 from datetime import datetime
 
@@ -13,19 +14,24 @@ timeframe = '15m'
 oclhvColumns = ['Unix','Open','Close','Low', 'High','Volume']
 finalColumns = []
 
-def readCSV(exchange,pair,timeframe):
+def read_CSV(exchange,pair,timeframe):
     df = pd.read_csv (f'data/fresh/{exchange}/{pair}_{timeframe}.csv', names=oclhvColumns)
     df['Date'] = pd.to_datetime(df['Unix'],unit='ms')
     df = df.set_index('Date')
     df = df[['Unix','Open','Close','Low', 'High','Volume']]
     return df
 
-def addTechInd(df,indicatorList) :
-    do = 'nothing'
-    return do
+def add_tech_ind(df) :
+    df['SMA12'] = TA.SMA(df, 12)
+    df['SMA21'] = TA.SMA(df, 21)
+    df['SMA55'] = TA.SMA(df, 55)
+    df['SMA100'] = TA.SMA(df, 100)
+    df['RSI'] = TA.RSI(df)
+    df['OBV'] = TA.OBV(df)
+    return df
 
 
-def pickle(exchange,pair,timeframe,df):
+def pickle_df(exchange,pair,timeframe,df):
     #IT WENT IN THE JAR HERE!!
     df.to_pickle(f'data/processed/{exchange}/{pair}_{timeframe}.pkl')
     print(f'data/processed/{exchange}/{pair}_{timeframe}.pkl')
@@ -34,8 +40,9 @@ def pickle(exchange,pair,timeframe,df):
 
 
 if __name__ == '__main__':
-    df = readCSV('Binance','btc_usdt','15m')
-    pickle('Binance','btc_usdt','15m',df)
+    df = read_CSV('Binance','btc_usdt','15m')
+    df = add_tech_ind(df)
+    pickle_df('Binance','btc_usdt','15m',df)
 
     df.info()
     print(df.head(1))
